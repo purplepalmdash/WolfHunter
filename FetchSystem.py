03bef1830f1310bce1057010704c2e5a8f737d8b
purplepalm <purplepalm@gmail.com>
2015-06-25 14:12:02 +0800

    Added more system infos at home, and added the initial readme.md, commit them

--- FetchSystem.py
+++ FetchSystem.py
@@ -14,9 +14,10 @@ import json
 CobblerServer = xmlrpclib.Server("http://127.0.0.1/cobbler_api")
 
 # Visit http://Your_URL/system will get all of the installed system node name
+# Todo: 2015_06_25: Use a template for beautifully displaying these items. 
 @route('/system')
 def list_system():
-	AllSystems = '' 
+	AllSystems = 'Only Displayed the eth0 Information Currently </br>'
 	for i in CobblerServer.get_systems():
 		# i in a dictionary, need to be prettified
 		# `print type(i)` result is <type 'dict'>
@@ -24,13 +25,18 @@ def list_system():
 
 		# Display all of the interfaces
 		#print json.dumps(i['interfaces'], sort_keys=True, indent=4) + "\n######\n"
-		print json.dumps(i['interfaces']['eth0']['ip_address'], sort_keys=True, indent=4) + "\n######\n"
+		#print json.dumps(i['interfaces']['eth0']['ip_address'], sort_keys=True, indent=4) + "\n######\n"
+		# Following displayed all of the information currently we are interested on one single node.
 		AllSystems += i['name'] + '\t' \
+				+ i['interfaces']['eth0']['mac_address'] + '\t' \
 				+ i['interfaces']['eth0']['ip_address'] + '\t' \
+				+ i['gateway']+ '\t' \
+				+ i['hostname']+ '\t' \
+				+ i['profile']+ '\t' \
+				+ i['interfaces']['eth0']['dns_name'] + '\t' \
+				+ str(i['ctime'])+ '\t' \
+				+ str(i['mtime'])+ '\t' \
 				+ '</br>'
-		#+ i['mac_address'] + '  '\
-		#+ ' </br>'
-		#AllSystems += '</br>'
 
 	print AllSystems
 	#AllSystems = CobblerServer.get_systems()[0]
@@ -39,4 +45,4 @@ def list_system():
 # Enable Debug
 debug(True)
 
-run(reloader=True, port=8848, host='10.47.58.2')
+run(reloader=True, port=8848, host='0.0.0.0')
